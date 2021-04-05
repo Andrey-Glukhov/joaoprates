a2b6a8b649a6e1749a7b69ae9358fdd9af5a4d7a
Eliza <gluhova.liza@gmail.com>
2021-04-05 09:12:58 +0300

    create

---
+++ functions.php
@@ -0,0 +1,44 @@
+<?php
+function jp_script_enqueue(){
+//css
+	wp_enqueue_style( 'jp-stylesheet', get_template_directory_uri() . '/css/joaoprates.css', array(), '1.0.0', 'all' );
+  //js
+  // unregister jQuery
+  wp_deregister_script('jquery-core');
+  wp_deregister_script('jquery');
+
+  // register
+  wp_register_script( 'jquery-core', 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js', false, null, true );
+  wp_register_script( 'jquery', false, array('jquery-core'), null, true );
+
+  // enqueue
+  wp_enqueue_script( 'jquery' );
+  // Bootstrap
+  wp_enqueue_script( 'bootstrap-js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js', array('jquery'), null, true );
+  
+  wp_enqueue_script( 'jp-js', get_template_directory_uri() . '/js/joaoprates.js', array('jquery', 'bootstrap-js'), null, true );
+
+  
+}
+add_action( 'wp_enqueue_scripts', 'jp_script_enqueue' );
+
+function jp_theme_setup(){
+  add_theme_support('menus');
+  register_nav_menu('primary', 'Primary Header Navigation');
+  register_nav_menu('footer_menu', 'Footer Navigation Area');
+}
+
+function mytheme_add_woocommerce_support() {
+  add_theme_support( 'woocommerce' );
+}
+add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
+
+add_action('init', 'jp_theme_setup');
+add_theme_support('custom-background');
+add_theme_support('custom-header');
+add_theme_support('post-formats', array('aside', 'chat', 'gallery','link','image','quote','status','video'));
+add_theme_support('post-thumbnails');
+
+
+
+?>
---
+++ header.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1" >
+  <meta name="keywords" content="Joao Prates, Joao Prates photographer, Joao Prates art photo, Joao Prates tulip photo, Joao Prates The Hague, Joao Prates the Netherlands, Joao Prates Den Haag, Joao Prates Amazon photo, Tulip Art, Tulip photo art, Golden Tulips, Tulips Art, Tulips photo art, Amazon art, Amazon photo art, Still life Tulip, Still life Amazon, Dutch masters, Dutch tulips, Still-life photo, Amazon still-life photo fruits and vegetables, Albert Eckhout" />
+  <meta name="description" content="Joao Prates Photography" />
+
+  <title>Joao Prates Photography</title>
+
+  <link href="https://fonts.googleapis.com/css2?family=Bitter:ital,wght@0,100;0,400;0,500;0,700;0,900;1,100;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+  
+  <?php wp_head(); ?>
+</head>
+
+<?php
+if(is_front_page()):
+  $jp_classes = array('jp_front_class', 'front_class');
+else:
+  $jp_classes = array('no_jp_front_class');
+endif;
+?>
+
+<body <?php body_class($jp_classes); ?>>
+  <header>
+  <?php include (TEMPLATEPATH . '/navigation.php'); ?>
+	</header>
