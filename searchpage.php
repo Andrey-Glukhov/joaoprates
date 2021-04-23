d02df543535424ac1605dad3134e69d84ee54b9e
Andrey-Glukhov <agi2000@mail.ru>
2021-04-23 12:05:21 +0300

    add search

---
+++ searchpage.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Template Name: Search Page
+ */
+
+?>
+<?php get_header(); ?>
+
+<div class="container page-padding">
+
+    <div class="row justify-content-end">
+        <div class="col-md-8 col-sm-12">
+            <main id="main" class="site-main" role="main">
+            <?php get_search_form(); ?>
+            </main><!-- #main -->
+        </div><!-- .col -->
+    </div><!-- .row -->
+</div><!-- .container -->
+
+<?php get_footer(); 
