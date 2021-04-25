343c6138ccd11d0296125b35ee8c58038ded2f98
Andrey-Glukhov <agi2000@mail.ru>
2021-04-25 10:02:21 +0300

    change attribute dropdown

--- functions.php
+++ functions.php
@@ -294,4 +294,78 @@ function bv_image_on_checkout( $name, $cart_item, $cart_item_key ) {
 
 }
 
+// Add buttons to variable product attribute selection
+add_filter( 'woocommerce_dropdown_variation_attribute_options_html', 'add_buttons_attr_dropdown', 10,2);
+
+function add_buttons_attr_dropdown($html, $args)  {
+
+	$args = wp_parse_args(
+		apply_filters( 'woocommerce_dropdown_variation_attribute_options_args', $args ),
+		array(
+			'options'          => false,
+			'attribute'        => false,
+			'product'          => false,
+			'selected'         => false,
+			'name'             => '',
+			'id'               => '',
+			'class'            => '',
+			'show_option_none' => __( 'Choose an option', 'woocommerce' ),
+		)
+	);
+
+	// Get selected value.
+	if ( false === $args['selected'] && $args['attribute'] && $args['product'] instanceof WC_Product ) {
+		$selected_key = 'attribute_' . sanitize_title( $args['attribute'] );
+		// phpcs:disable WordPress.Security.NonceVerification.Recommended
+		$args['selected'] = isset( $_REQUEST[ $selected_key ] ) ? wc_clean( wp_unslash( $_REQUEST[ $selected_key ] ) ) : $args['product']->get_variation_default_attribute( $args['attribute'] );
+		// phpcs:enable WordPress.Security.NonceVerification.Recommended
+	}
+
+	$options               = $args['options'];
+	$product               = $args['product'];
+	$attribute             = $args['attribute'];
+	$name                  = $args['name'] ? $args['name'] : 'attribute_' . sanitize_title( $attribute );
+	$id                    = $args['id'] ? $args['id'] : sanitize_title( $attribute );
+	$class                 = $args['class'];
+	$show_option_none      = (bool) $args['show_option_none'];
+	$show_option_none_text = $args['show_option_none'] ? $args['show_option_none'] : __( 'Choose an option', 'woocommerce' ); // We'll do our best to hide the placeholder, but we'll need to show something when resetting options.
+
+	if ( empty( $options ) && ! empty( $product ) && ! empty( $attribute ) ) {
+		$attributes = $product->get_variation_attributes();
+		$options    = $attributes[ $attribute ];
+	}
+
+	$buttons_html  = '';	
+
+	if ( ! empty( $options ) ) {
+		if ( $product && taxonomy_exists( $attribute ) ) {
+			// Get terms if this is a taxonomy - ordered. We need the names too.
+			$terms = wc_get_product_terms(
+				$product->get_id(),
+				$attribute,
+				array(
+					'fields' => 'all',
+				)
+			);
+
+			foreach ( $terms as $term ) {
+				if ( in_array( $term->slug, $options, true ) ) {
+					$buttons_html .= '<div class="attrib_selection" data-value="' . esc_attr( $term->slug ) . '" ' . selected( sanitize_title( $args['selected'] ), $term->slug, false ) . '>' . esc_html( apply_filters( 'woocommerce_variation_option_name', $term->name, $term, $attribute, $product ) ) . '</div>';
+				}
+			}
+		} else {
+			foreach ( $options as $option ) {
+				// This handles < 2.4.0 bw compatibility where text attributes were not sanitized.
+				$selected = sanitize_title( $args['selected'] ) === $args['selected'] ? selected( $args['selected'], sanitize_title( $option ), false ) : selected( $args['selected'], $option, false );
+				$buttons_html    .= '<div class="attrib_selection" data-value="' . esc_attr( $option ) . '" ' . $selected . '>' . esc_html( apply_filters( 'woocommerce_variation_option_name', $option, null, $attribute, $product ) ) . '</div>';
+			}
+		}
+	}
+
+	$html .= $buttons_html;
+	return $html;
+
+}
+ 
+
 ?>
--- js/joaoprates.js
+++ js/joaoprates.js
@@ -4,52 +4,18 @@ jQuery(function($) {
         $("[name='update_cart']").trigger("click");
     });
     $(window).on("load", function() {
-        //$('.wp-block-jetpack-slideshow_button-next').css('top', '');
-        // $('.wp-block-jetpack-slideshow_button-next').on('change', function(e) {
-        //     console.log(e);
-        // });
-        // var target = document.querySelector('.wp-block-jetpack-slideshow_button-next');
-
-        // // создаем новый экземпляр наблюдателя
-        // var observer = new MutationObserver(function(mutations) {
-        //     mutations.forEach(function(mutation) {
-        //         console.log(mutation.type);
-        //     });
-        // });
-
-        // // создаем конфигурации для наблюдателя
-        // var config = { attributes: true, childList: true, characterData: true };
-
-        // // запускаем механизм наблюдения
-        // observer.observe(target, config);
-
-        // // позже, если надо, прекращаем наблюдение
-        // //observer.disconnect();
-
-        // var tts = setTimeout(function() {
-        //     document.querySelector('.wp-block-jetpack-slideshow_button-next').style.top = '';
-        // }, 3000);
+      
         $('#carouselExampleSlidesOnly').slick({
             slidesToShow: 1,
             slidesToScroll: 1,
             autoplay: false,
             autoplaySpeed: 2000,
-            //arrows: false,
-            //adaptiveHeight: true
-            //variableWidth: true
-
         }).on('setPosition', function(event, slick) {
-            // var $slideImages = slick.$slideTrack.find('.slick-slide img');
-            // console.log($slideImages);
-            // if ($slideImages.first()[0].clientHeight >= $slideImages.first()[0].naturalHeight) return;
-            // $slideImages.height('auto');
-            // $slideImages.width('100%');
-            // var imgHeights = $slideImages.map(function() {
-            //     return this.clientHeight;
-            // }).get();
-            // var maxHeight = Math.max.apply(null, imgHeights);
-            // $slideImages.height(maxHeight);
-            // $slideImages.width('auto');
+        
         });
     });
+    $(".attrib_selection").on('click', function () {
+        var tagValue = $(this).data('value');
+        $('select#size').val(tagValue).trigger("change");
+    })
 });
