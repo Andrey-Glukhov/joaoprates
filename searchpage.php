efc8e60b69b3676e8d945f50661bc82283b1586a
Andrey-Glukhov <agi2000@mail.ru>
2021-04-29 15:27:04 +0300

    change css

--- searchpage.php
+++ searchpage.php
@@ -8,7 +8,9 @@
  get_header(); ?>
 
 <div class="container page-padding search_container">
-    <main id="main" class="site-main" role="main">
+	<div class="row justify-content-center">
+		<div class="col-lg-6 col-md-8 col-sm-10 col-11">
+			    <main id="main" class="site-main" role="main">
          
         <form role="search" method="get" id="searchform" class="searchform" action="<?php echo get_permalink(); ?>">
             <div>
@@ -19,45 +21,51 @@
         </form> 
       
     </main><!-- #main -->
-    <div class="row justify-content-senter">
-        <div class="col-md-8 col-sm-12">
-            <div  class="search_results" role="main">
-            <ul class="search_result_list">
+			
+		</div>
+	</div>
+
+    <div class="row justify-content-center">
             <?php 
          
-         if( isset( $_REQUEST['search'] ) ){
-            $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-            $args = array(
-                'paged'           => $paged,
-                'posts_per_page'  => -1, 
-                //'post_type'       => 'post', 
-                's'               => $_REQUEST[ 'search' ]
-            );
-      
+            if( isset( $_REQUEST['search'] ) ){
+                $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+                $args = array(
+                    'paged'           => $paged,
+                    'posts_per_page'  => -1, 
+                    //'post_type'       => 'post', 
+                    's'               => $_REQUEST[ 'search' ]
+                );
+        
 
-            $search = new WP_Query($args);
-            if ( $search->have_posts() ) { ?>
-                <h2>Search for: <?php echo $_REQUEST[ 'search' ]; ?> </h2>  
-                <div>Found <?php echo $search->post_count; ?> results</div>  
-                <?php while ( $search->have_posts() ) {
-                    $search->the_post();
-                    echo '<li>';
-                    $thumbnail_link = get_the_post_thumbnail_url(); 
-                // error_log('search1---' . print_r($post,true));
-                    if ($thumbnail_link) {
-                        echo'<img src="' . $thumbnail_link . '" />';
-                    } else {
-                        echo'<img src="' . wc_placeholder_img_src() . '" />';
-                        
-                    }
-                    echo '<div class="search_link"><a href="' . get_the_permalink() . '">' . get_the_title() . '</a></div></li>';
-                } 
-            }?>
-            </ul>
-        </div><!-- #main -->
-        </div><!-- .col -->
+                $search = new WP_Query($args);
+                if ( $search->have_posts() ) { ?>
+			<div class="col-12 search_title_wrapper"><h1 class="about_header">Search for: <?php echo $_REQUEST[ 'search' ]; ?> </h1>  
+				<div>Found <?php echo $search->post_count; ?> results</div>
+		</div>
+		</div><!-- .row -->
+                      
+		<div class="row justify-content-center">
+        
+                    <?php while ( $search->have_posts() ) {
+                        $search->the_post();
+                        echo '<div class="col-md-4 col-sm-10 col-11">';
+                        $thumbnail_link = get_the_post_thumbnail_url(); 
+                    // error_log('search1---' . print_r($post,true));
+                        if ($thumbnail_link) {
+                            echo'<img class="find_image" src="' . $thumbnail_link . '" />';
+                        } else {
+                            echo'<img class="find_image" src="' . wc_placeholder_img_src() . '" />';
+                            
+                        }
+                        echo '<div class="search_link"><a href="' . get_the_permalink() . '">' . get_the_title() . '</a></div></div>';
+                    } 
+                }
+            } ?>
+        
+
     </div><!-- .row -->
-    <?php } ?>
+   
 </div><!-- .container -->
 
 <?php get_footer(); 
