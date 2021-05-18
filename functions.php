ac3caed818d2289b8fdba8ef2e2fa7854efc45e1
Andrey-Glukhov <agi2000@mail.ru>
2021-05-18 12:54:25 +0300

    change functions

--- functions.php
+++ functions.php
@@ -41,10 +41,12 @@ function mytheme_add_woocommerce_support() {
   add_theme_support( 'woocommerce' );
 }
 add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
+// Change Woocommerce pages
 remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10 );
 remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 20 );
 remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10 );
 remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20 );
+
 add_action( 'woocommerce_after_single_product_summary', 'woocommerce_cross_sell_display', 40 );
 
 //remove_action( 'woocommerce_after_shop_loop', 'woocommerce_pagination', 10 );
@@ -52,6 +54,8 @@ remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wra
 
 // remove_action( 'woocommerce_before_single_product_summary', 'woocommerce_show_product_images', 20 );
 // add_action( 'woocommerce_before_single_product_summary', 'bv_show_product_image', 20 );
+// Remove prices from product pages
+remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10 );
 
 function bv_show_product_image()  {
 	
